58ee6926c05b6593186a473d4b945952fa6bae9c
Sam Keller <samskeller@gmail.com>
2012-04-23 19:20:01 -0500

    adding dbconstants file with deleted mysql account information

---
+++ dbconstants.py
@@ -0,0 +1,9 @@
+# Information needed to login to SQL
+#
+# Don't look!
+# 
+# Justin Norden, Cole Stephan, Sam Keller
+
+host = 'localhost'
+# need to set the user and passwd for the mysql database
+db= 'kellers'
